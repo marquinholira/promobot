f6d963bf2b4cbe4d0aea4576ba454bd8f64dc165
marquinholira <email.lira@yahoo.com.br>
2026-08-05 10:51:50 -0300

    Add buscar function to retrieve promotions from Mercado Livre

---
+++ fontes/mercadolivre.py
@@ -0,0 +1,27 @@
+def buscar():
+
+    return [
+
+        {
+            "fonte": "Mercado Livre",
+
+            "titulo": "Cupom R$50 OFF",
+
+            "cupom": "MELI50",
+
+            "link": "https://mercadolivre.com.br"
+
+        },
+
+        {
+            "fonte": "Mercado Livre",
+
+            "titulo": "Frete Grátis",
+
+            "cupom": "",
+
+            "link": "https://mercadolivre.com.br"
+
+        }
+
+    ]
