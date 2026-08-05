b7bb610fc4d971ae71e1e68c45ccac5ed9d2cb58
marquinholira <email.lira@yahoo.com.br>
2026-08-05 10:38:34 -0300

    Add buscar_promocoes function for promotions

---
+++ fontes.py
@@ -0,0 +1,23 @@
+def buscar_promocoes():
+
+    return [
+
+        {
+            "titulo": "Cupom Mercado Livre R$50 OFF",
+
+            "cupom": "MELI50",
+
+            "categoria": "Mercado Livre"
+
+        },
+
+        {
+            "titulo": "Notebook Lenovo",
+
+            "cupom": "",
+
+            "categoria": "Mercado Livre"
+
+        }
+
+    ]
